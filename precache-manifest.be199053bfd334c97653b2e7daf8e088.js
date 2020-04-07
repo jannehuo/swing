5ad06d160142c8b4fdd7a415439e2f1c2070662e
Janne <janne@richen.fi>
2020-04-07 19:50:13 +0300

    Updates

---
+++ precache-manifest.be199053bfd334c97653b2e7daf8e088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbe98ca717dccb57e939b22e67235eb",
+    "url": "/swing/index.html"
+  },
+  {
+    "revision": "17271dc74ea4b8931477",
+    "url": "/swing/static/css/main.ce67f4fc.chunk.css"
+  },
+  {
+    "revision": "0350cf5b6bcc47269517",
+    "url": "/swing/static/js/2.80f5644d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swing/static/js/2.80f5644d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17271dc74ea4b8931477",
+    "url": "/swing/static/js/main.7f51b402.chunk.js"
+  },
+  {
+    "revision": "b47b6e95fe423b671f2c",
+    "url": "/swing/static/js/runtime-main.2cd2b849.js"
+  }
+]);
