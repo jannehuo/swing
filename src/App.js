a679fbb2a223dc048e1e15a9cad100c64a69506f
Janne <janne@richen.fi>
2020-04-07 20:14:44 +0300

    mobile styles for nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Start from "./components/Start";
 import "./App.css";
 
 function App() {
+  const [navOpen, toggleNav] = useState(false);
+
+  const toggle = e => {
+    e.preventDefault();
+    toggleNav(!navOpen);
+  };
+
+  const navClass = navOpen ? "open" : "";
+  console.log(navClass);
   return (
     <React.Fragment>
-      <nav>
+      <nav className={navClass}>
         <div className="nav-top">
           <h1 className="app-header">swings</h1>
           <ul className="nav-links">
@@ -32,7 +41,11 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main></main>
+      <main>
+        <button onClick={toggle} className="btn btn--menu">
+          {navOpen ? "close" : "menu"}
+        </button>
+      </main>
     </React.Fragment>
   );
 }
