7cd9b4b058a3298e27b6a667df08411c17d2ae5b
Huotari Janne <huotaja@HFMAC86ADMINs-MacBook-Pro.local>
2021-06-01 15:55:44 +0300

    fixed bug when front page was not showing anything if program was empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import * as React from "react";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
-import Nav from "./components/Nav";
-import Home from "./views/Home";
-import Edit from "./views/Edit";
-import New from "./views/New";
-import { get, save } from "./utils/storage";
-import { isEmptyObject } from "./utils/";
-import { Iprogram } from "./interfaces";
-import AppContext from "./context";
-import { DEFAULT_LOCALE } from "./constants";
-import { getLocales, switchLocale } from "./localizations";
-import "./App.scss";
+import * as React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './views/Home'
+import Edit from './views/Edit'
+import New from './views/New'
+import { get, save } from './utils/storage'
+import { isEmptyObject } from './utils/'
+import { Iprogram } from './interfaces'
+import AppContext from './context'
+import { DEFAULT_LOCALE } from './constants'
+import { getLocales, switchLocale } from './localizations'
+import './App.scss'
 
 const App: React.SFC<{}> = () => {
-  const [program, setProgram] = React.useState<Iprogram | null>(null);
-  const [localizations, setLocalizations] = React.useState<Object>({});
+  const [program, setProgram] = React.useState<Iprogram | null>(null)
+  const [localizations, setLocalizations] = React.useState<Object>({})
   React.useEffect(() => {
-    setProgram(get("program"));
-    const storedLocale = localStorage.getItem("locale");
-    const locale = storedLocale || DEFAULT_LOCALE;
-    setLocalizations(getLocales(locale));
-  }, []);
+    setProgram(get('program'))
+    const storedLocale = localStorage.getItem('locale')
+    const locale = storedLocale || DEFAULT_LOCALE
+    setLocalizations(getLocales(locale))
+  }, [])
 
   const update = (values: Iprogram) => {
-    setProgram(values);
-    save("program", values);
-  };
+    setProgram(values)
+    save('program', values)
+  }
 
   const changeLang = (locale: string) => {
-    const newLocale = switchLocale(locale);
-    setLocalizations(newLocale);
-  };
-  if (isEmptyObject(localizations) || !program) {
-    return null;
+    const newLocale = switchLocale(locale)
+    setLocalizations(newLocale)
+  }
+  if (isEmptyObject(localizations)) {
+    return null
   }
 
   return (
@@ -50,7 +50,7 @@ const App: React.SFC<{}> = () => {
         </AppContext.Provider>
       </Router>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
